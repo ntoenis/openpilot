b24411fbb8975932ba8ebcaf0608035a636add94
Chris Souers <csouers@gmail.com>
2020-07-29 14:49:17 -0400

    reset controlsd

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -199,8 +199,6 @@ class Controls:
     elif self.sm['pathPlan'].laneChangeState in [LaneChangeState.laneChangeStarting,
                                                  LaneChangeState.laneChangeFinishing]:
       self.events.add(EventName.laneChange)
-      if self.sm['pathPlan'].laneChangeState in [LaneChangeState.laneChangeStarting:
-
 
     if self.can_rcv_error or (not CS.canValid and self.sm.frame > 5 / DT_CTRL):
       self.events.add(EventName.canError)
@@ -247,7 +245,7 @@ class Controls:
       self.events.add(EventName.noTarget)
 
   def data_sample(self):
-    # """Receive data from sockets and update carState"""
+    """Receive data from sockets and update carState"""
 
     # Update carState from CAN
     can_strs = messaging.drain_sock_raw(self.can_sock, wait_for_one=True)
@@ -277,7 +275,7 @@ class Controls:
     return CS
 
   def state_transition(self, CS):
-    #"""Compute conditional state transitions and execute actions on state transitions"""
+    """Compute conditional state transitions and execute actions on state transitions"""
 
     self.v_cruise_kph_last = self.v_cruise_kph
 
@@ -354,7 +352,7 @@ class Controls:
     self.enabled = self.active or self.state == State.preEnabled
 
   def state_control(self, CS):
-    # """Given the state, this function returns an actuators packet"""
+    """Given the state, this function returns an actuators packet"""
 
     plan = self.sm['plan']
     path_plan = self.sm['pathPlan']
@@ -404,7 +402,7 @@ class Controls:
     return actuators, v_acc_sol, a_acc_sol, lac_log
 
   def publish_logs(self, CS, start_time, actuators, v_acc, a_acc, lac_log):
-    # """Send actuators and hud commands to the car, send controlsstate and MPC logging"""
+    """Send actuators and hud commands to the car, send controlsstate and MPC logging"""
 
     CC = car.CarControl.new_message()
     CC.enabled = self.enabled
