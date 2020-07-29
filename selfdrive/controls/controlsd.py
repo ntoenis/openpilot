df39ae9448e957c3376a21c7c10338a8da49d379
Chris Souers <csouers@gmail.com>
2020-07-29 15:07:39 -0400

    update for cereal change.

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -9,7 +9,7 @@ from common.params import Params, put_nonblocking
 import cereal.messaging as messaging
 from selfdrive.config import Conversions as CV
 from selfdrive.boardd.boardd import can_list_to_can_capnp
-from selfdrive.car.car_helpers import get_car, get_startup_event
+from selfdrive.car.car_helpers import get_car, get_startup_event, get_one_can
 from selfdrive.controls.lib.lane_planner import CAMERA_OFFSET
 from selfdrive.controls.lib.drive_helpers import update_v_cruise, initialize_v_cruise
 from selfdrive.controls.lib.longcontrol import LongControl, STARTING_TARGET_SPEED
