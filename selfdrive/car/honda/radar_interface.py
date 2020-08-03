b7286f5cf9dab6ceed0bd883298ddd04b0b8d405
Chris Souers <csouers@gmail.com>
2020-08-03 14:16:58 -0400

    more autohigh beam removal

--- selfdrive/car/honda/radar_interface.py
+++ selfdrive/car/honda/radar_interface.py
@@ -183,9 +183,12 @@ class RadarInterface(RadarInterfaceBase):
       # radar point only valid if it's a valid measurement and score is above 50
       # bosch radar data needs to match Index and Index2 for validity
       # also for now ignore construction elements
-      if (cpt['Valid'] or cpt['Tracked'])and (cpt['LongDist']>0) and (cpt['LongDist'] < BOSCH_MAX_DIST) and \
+      # if (cpt['Valid'] or cpt['Tracked'])and (cpt['LongDist']>0) and (cpt['LongDist'] < BOSCH_MAX_DIST) and \
+      #     (self.valid_cnt[message] > VALID_MESSAGE_COUNT_THRESHOLD) and (cpt['ProbExist'] >= OBJECT_MIN_PROBABILITY) and \
+      #     (cpt2['Class'] < 4) and ((cpt['LongSpeed'] >= AHB_STATIONARY_MARGIN - v_ego) or (v_ego < 2)):
+      if (cpt['Valid'] or cpt['Tracked'])and (cpt['LongDist']> 0) and (cpt['LongDist'] < BOSCH_MAX_DIST) and \
           (self.valid_cnt[message] > VALID_MESSAGE_COUNT_THRESHOLD) and (cpt['ProbExist'] >= OBJECT_MIN_PROBABILITY) and \
-          (cpt2['Class'] < 4) and ((cpt['LongSpeed'] >= AHB_STATIONARY_MARGIN - v_ego) or (v_ego < 2)):
+          (cpt2['Class'] < 4):
         if message not in self.pts and (cpt['Tracked']):
           self.pts[message] = car.RadarData.RadarPoint.new_message()
           self.pts[message].trackId = self.trackId
@@ -243,6 +246,7 @@ if __name__ == "__main__":
     # ret,retext,ahb = RI.update(can_strings = None, v_ego = 0.)
     # print(chr(27) + "[2J")
     # print(ret,retext)
-    ret = RI.update(can_strings = None, v_ego = 0.)
+    # ret = RI.update(can_strings = None, v_ego = 0.)
+    ret = RI.update(can_strings = None)
     print(chr(27) + "[2J")
     print(ret)
