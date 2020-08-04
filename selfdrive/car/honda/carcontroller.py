59d3b63ed2366e4b3a7607be769f131f552d1893
Chris Souers <csouers@gmail.com>
2020-08-04 13:37:11 -0400

    Update carcontroller.py

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -230,6 +230,7 @@ class CarController():
     if self.useTeslaRadar:
       if (frame % 100 == 0):
         can_sends.append(teslaradarcan.create_radar_VIN_msg(self.radarVin_idx, str(self.radarVin), self.radarBus, self.radarTriggerMessage, self.useTeslaRadar, int(self.radarPosition), int(self.radarEpasType)))
+        print("sending tesla vin msg")
         self.radarVin_idx += 1
         self.radarVin_idx = self.radarVin_idx % 3
 
