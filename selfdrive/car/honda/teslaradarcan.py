9cfd222b7e6d9f088324196d86577c651c4a5f2c
Chris Souers <csouers@gmail.com>
2020-08-03 22:11:19 -0400

    Update teslaradarcan.py

--- selfdrive/car/honda/teslaradarcan.py
+++ selfdrive/car/honda/teslaradarcan.py
@@ -41,7 +41,11 @@ def create_radar_VIN_msg(radarId, radarVIN, radarCAN, radarTriggerMessage, useRa
   msg_len = 8
   msg = create_string_buffer(msg_len)
   if radarId == 0:
-    struct.pack_into('BBBBBBBB', msg, 0, radarId, radarCAN, useRadar + (radarPosition << 1) + (radarEpasType << 3), int((radarTriggerMessage >> 8) & 0xFF), (radarTriggerMessage & 0xFF), ord(radarVIN[0]), ord(radarVIN[1]), ord(radarVIN[2]))
+    print(type(useRadar))
+    print(type(radarPosition))
+    print(type(radarEpasType))
+    dat1 = int(useRadar) + int(radarPosition << 1) + int(radarEpasType << 3)
+    struct.pack_into('BBBBBBBB', msg, 0, radarId, radarCAN, dat1, int((radarTriggerMessage >> 8) & 0xFF), int(radarTriggerMessage & 0xFF), ord(radarVIN[0]), ord(radarVIN[1]), ord(radarVIN[2]))
   if radarId == 1:
     struct.pack_into('BBBBBBBB', msg, 0, radarId, ord(radarVIN[3]), ord(radarVIN[4]), ord(radarVIN[5]), ord(radarVIN[6]), ord(radarVIN[7]), ord(radarVIN[8]), ord(radarVIN[9]))
   if radarId == 2:
