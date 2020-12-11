57b947a031b385a0d99070e1f5425d1a14b6d88d
cfranhonda <46506059+cfranhonda@users.noreply.github.com>
2020-12-11 14:44:12 -0800

    remove relay

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -128,10 +128,8 @@ class CarInterface(CarInterfaceBase):
   #   return float(max(max_accel, a_target / A_ACC_MAX)) * min(speedLimiter, accelLimiter)
 
   @staticmethod
-  def get_params(candidate, fingerprint=gen_empty_fingerprint(), has_relay=False, car_fw=[]):  # pylint: disable=dangerous-default-value
-    params = Params()
-
-    ret = CarInterfaceBase.get_std_params(candidate, fingerprint, has_relay)
+  def get_params(candidate, fingerprint=gen_empty_fingerprint(), car_fw=[]):  # pylint: disable=dangerous-default-value
+    ret = CarInterfaceBase.get_std_params(candidate, fingerprint)
     ret.carName = "honda"
     ret.safetyParam = 0
 
