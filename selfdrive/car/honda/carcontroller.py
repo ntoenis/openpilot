98745c08ede60c76d8cb0dd13d5151ad70f90ab6
cfranhonda <46506059+cfranhonda@users.noreply.github.com>
2020-12-12 07:05:27 -0800

    Update carcontroller.py

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -167,8 +167,8 @@ class CarController():
       # TODO: radar disable hacked together to see if it works
       if (frame % 10) == 0:
         # tester present - w/ no response (keeps radar disabled)
-        can_sends.append([0x18DAB0F1, 0, b"\x02\x3E\x80\x00\x00\x00\x00\x00", 1 if CS.CP.isPandaBlack else 0])
-
+        can_sends.append([0x18DAB0F1, 0, b"\x02\x3E\x80\x00\x00\x00\x00\x00", 1]) #force 1 because i took out CS.CP.isPandaBlack down below
+ 
     # Send steering command.
     idx = frame % 4
     can_sends.append(hondacan.create_steering_control(self.packer, apply_steer,
