49ac837e953aa307801c9d845f885bebc5c174ef
cfranhonda <46506059+cfranhonda@users.noreply.github.com>
2020-12-12 18:34:30 -0800

    Update carcontroller.py

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -99,7 +99,8 @@ class CarController():
     self.packer = CANPacker(dbc_name)
     self.new_radar_config = False
     self.radarVin_idx = 0
-    self.RadarTriggerMessage = 0x94
+    self.radarTriggerMessage = 0x94
+    self.useTeslaRadar = 0
 
     self.params = CarControllerParams(CP)
 
